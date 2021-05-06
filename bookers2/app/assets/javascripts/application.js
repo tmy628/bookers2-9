ef78fac02ee32c16ebe21bb71de0cb084e616659
tmy628 <tmy6h1r@gmail.com>
2021-05-06 05:44:52 +0000

    turbolinksを無効化

--- bookers2/app/assets/javascripts/application.js
+++ bookers2/app/assets/javascripts/application.js
@@ -19,3 +19,18 @@
 //= require turbolinks
 //= require jquery3
 //= require_tree .
+
+$(function () {
+  $(document).on('turbolinks:load', () => {
+    $('#user_postcode').jpostal({
+      postcode: [
+        '#user_postcode'
+      ],
+      address: {
+        "#user_prefecture_code": "%3",
+        "#user_address_city": "%4%5",
+        "#user_address_street": "%6%7"
+      }
+    });
+  });
+});
--- bookers2/app/controllers/chats_controller.rb
+++ bookers2/app/controllers/chats_controller.rb
@@ -27,5 +27,4 @@ class ChatsController < ApplicationController
   def chat_params
     params.require(:chat).permit(:message, :room_id)
   end
-
 end
